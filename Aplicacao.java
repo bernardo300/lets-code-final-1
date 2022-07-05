13892b006440531e848ba8c940898eaf8947db99
Anamdib <102883363+Anamdib@users.noreply.github.com>
2022-07-05 18:36:05 -0300

    Add files via upload

--- Aplicacao.java
+++ Aplicacao.java
@@ -38,16 +38,25 @@ public class Aplicacao {
             System.out.println("teste do equal: " + (Objects.equals(tipoCliente, "10")));
 
             if(tipoCliente.equals("10")){
-                System.out.println("Informe tipo de conta: 1 para Conta Corrente, 2 Conta Investimento");
+                System.out.println(" ----------------------------------------");
+                System.out.println("Informe o tipo de conta desejado: ") ;
+                System.out.println("  1: para Conta Corrente");
+                System.out.println("  2: para Conta investimento");
+                System.out.println(" ----------------------------------------");
                 tipoConta = sc.next().trim();
-           //     tipoConta = Integer.toString(tipoConta);
+           
                 if(tipoConta.equals("1") && tipoConta.equals("2")) {   
                     System.out.println("Pessoas jurídicas não podem ter conta poupança");
                     System.exit(0);
                 }
                 tipoCliente = "PJ";
             } else {
-                System.out.println("Informe tipo de conta: 1 para Conta Corrente, 2 Conta Investimento, 3 Conta Poupança");
+                System.out.println(" ----------------------------------------");
+                System.out.println("Informe o tipo de conta desejado: ") ;
+                System.out.println("  1: para Conta Corrente");
+                System.out.println("  2: para Conta investimento");
+                System.out.println("  3: para Conta poupançca");
+                System.out.println(" ----------------------------------------");
                 tipoConta = sc.next().trim();
                 if(!(tipoConta.equals("1")) && !(tipoConta.equals("2")) && !(tipoConta.equals("3")) ) {
                     System.out.println("opçao inválida");
@@ -56,12 +65,19 @@ public class Aplicacao {
                 tipoCliente = "PF";
                 System.out.println(" tipo cliente: " + tipoCliente);
             }
+                System.out.println(" ----------------------------------------");
+                System.out.println("Informe o tipo de operação: ") ;
+                System.out.println("  1: para depósito");
+                System.out.println("  2: para saque");
+                System.out.println("  3: para transferência");
+                System.out.println(" ----------------------------------------");
+                operacao = sc.next().trim();
+                System.out.println(" ");
+                System.out.println("-----------------------------------------------");
+                System.out.println("informe valor a transacionar. Exemplo: 9999,99");
+                System.out.println("-----------------------------------------------");
 
-            System.out.println("Informe tipo de operaçao: 1 para depósito, 2 para saque, 3 para transferência");
-            operacao = sc.next().trim();
-            System.out.println("informe valor a transacionar. Exemplo: 9999,99");
-            valor = sc.nextBigDecimal();
-
+                valor = sc.nextBigDecimal();
 
             switch (tipoConta) {
                 case "1":
@@ -86,7 +102,7 @@ public class Aplicacao {
         throws MensagemErro {
         ContaCorrente cc = new ContaCorrente(agencia, conta);
         cc.setTipoCliente(tipoCliente);
-        System.out.println("entrou em CC");
+        System.out.println("entrou em CC" + "  " + cc.getTipoCliente());
 
         BigDecimal deposito = new BigDecimal(5000.00);    // lixo
         cc.depositar(deposito);                                // lixo
