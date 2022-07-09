dfaa98802fa376ba4bc088f47127eba58292b0fb
Bernardo Lopes de Aguiar Filho <bernardolopes.luz@hotmail.com>
2022-07-09 16:58:12 -0300

    refatoração

--- src/main/java/br/com/mybank/models/conta/ContaPoupanca.java
+++ src/main/java/br/com/mybank/models/conta/ContaPoupanca.java
@@ -9,22 +9,17 @@ import br.com.mybank.models.cliente.ClientePessoaFisica;
 
 public class ContaPoupanca extends Conta implements OperacaoInvestir, OperacaoConsultaSaldo{
     private BigDecimal taxaRendimento = new BigDecimal(1.5);
-    private BigDecimal juros = new BigDecimal(0);
-    private BigDecimal saldoPoupanca = new BigDecimal(0);
 
     public ContaPoupanca(String agencia, int conta, ClientePessoaFisica titular){
         super(agencia, conta, titular);
     }
 
     public BigDecimal jurosPoupanca(){
-        this.saldoPoupanca = super.getSaldo();
-        this.juros = this.saldoPoupanca.multiply(this.taxaRendimento);
-        return this.juros;
+        return super.getSaldo().multiply(this.taxaRendimento);
     }
 
     public BigDecimal getsaldoPoupancaComJuros() {
-        this.saldoPoupanca = this.saldoPoupanca.add(jurosPoupanca());
-        return saldoPoupanca;
+        return super.getSaldo().add(jurosPoupanca());
     }
 
     public BigDecimal getsaldoPoupancaSemJuros() {
@@ -37,21 +32,15 @@ public class ContaPoupanca extends Conta implements OperacaoInvestir, OperacaoCo
     public void setTaxaRendimento(BigDecimal taxa) {
         this.taxaRendimento = taxaRendimento.divide(new BigDecimal(100));   
     }
-    public BigDecimal getJuros() {
-        return this.juros;
-    }
-    public void setJuros(BigDecimal juros) {
-        this.juros = juros;
-    }
 
     @Override
     public BigDecimal investir() {
-
-        return  getSaldo().multiply(this.taxaRendimento.divide(new BigDecimal(100))).add(getSaldo()).setScale(2,RoundingMode.DOWN);
+        setSaldo(getSaldo().multiply(this.taxaRendimento.divide(new BigDecimal(100))).add(getSaldo()).setScale(2,RoundingMode.DOWN));
+        return getSaldo();
     }
 
     @Override
     public BigDecimal consultarSaldo() {
-        return juros;
+        return getSaldo();
     }
 }
