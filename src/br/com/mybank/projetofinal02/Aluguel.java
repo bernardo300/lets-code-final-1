8bc73935263c46c46fc415c5a37fb1401e62e15b
Anamdib <102883363+Anamdib@users.noreply.github.com>
2022-07-26 17:11:24 -0300

    Add files via upload

--- src/br/com/mybank/projetofinal02/Aluguel.java
+++ src/br/com/mybank/projetofinal02/Aluguel.java
@@ -5,23 +5,23 @@ import java.math.BigDecimal;
 public class Aluguel {
     BigDecimal valorTotal = BigDecimal.ZERO;
     Integer diarias;
-    Cliente cli;
-    Veiculo veic;
+    Cliente cliente;
+    Veiculo veiculo;
    
     public Aluguel (Cliente cliente, Veiculo veiculo, Integer diarias){
-       cli = cliente;
-       veic = veiculo;
+       this.cliente = cliente;
+       this.veiculo = veiculo;
        this.diarias = diarias;
     }
 
     public void calculaAluguel(){
-        cli.getDesconto(diarias);
-        System.out.println("desconto: " + cli.getDesconto(diarias));
-        veic.getDiariaAluguel();
-        System.out.println("diariaaluguel: " + veic.getDiariaAluguel());
+        cliente.getDesconto(diarias);
+        System.out.println("desconto: " + cliente.getDesconto(diarias));
+        veiculo.getDiariaAluguel();
+        System.out.println("diariaaluguel: " + veiculo.getDiariaAluguel());
 
-        valorTotal = new BigDecimal(this.diarias).multiply(veic.getDiariaAluguel()).multiply
-          (new BigDecimal(1).subtract(cli.getDesconto(diarias)));
+        valorTotal = new BigDecimal(this.diarias).multiply(veiculo.getDiariaAluguel()).multiply
+          (new BigDecimal(1).subtract(cliente.getDesconto(diarias)));
         System.out.println("total do aluguel: " + valorTotal);
     }
 }
